5a25c93d7741247eac6e4e7a75b8d6f283975cb9
One_True <60509431+onetrue-6657@users.noreply.github.com>
2024-11-18 00:06:12 -0500

    ghpage

--- Major-Project/vite.config.js
+++ Major-Project/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/explore-minority-rights/', // 这是 GitHub Pages 的路径
   plugins: [
     vue(),
     vueDevTools(),
