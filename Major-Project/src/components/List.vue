10068b1af4d59267c5ffd897018d52ad4c696eee
One_True <60509431+onetrue-6657@users.noreply.github.com>
2024-11-19 22:54:35 -0500

    tutorial

--- Major-Project/src/components/List.vue
+++ Major-Project/src/components/List.vue
@@ -5,29 +5,47 @@
     
     <div class="list">
       <h1 class="green">List of Sources & Annotations</h1>
-        <p>For this part of the list and annotation of important ideas and sources, I will cover some significant materials that would be helpful for the research on this topic. This part will be divided into two sections. The first are archives that contain rich, valuable, and massive primary sources, which will be very important and useful for research. The second part will be articles, especially scholarly research articles, on this topic. The perspectives of different scholars will give us a new and different opinion, which may give us new inspiration.</p>
-        <br />
-        <p>My research on ethnic minority group rights began from the civil rights movement archive, <a href = "https://www.crmvet.org/">https://www.crmvet.org/</a>. This archive includes a huge amount of primary and secondary sources of this remarkable movement. It was created and maintained by a group of civil rights movement veterans, ensuring that these materials are relatively reliable and valuable. The archive includes four general categories: movement, activists, video/audio, and resources. And within these general categories, photo albums, videos, audio, reports, articles, letters, etc. are provided. To create a gallery wall focusing on the civil rights movement, I selected two law bills, a photo, and a primary report, focusing on the protest of racial segregation. Among these, the most important and direct is the photo, which shows a black girl marching with other people with the sentence “Do not buy segregation”, when protesting a racist local company. The visual impact that such a picture with large characters can be huge. The two law bills include the South segregation laws (also known as Jim Crow Laws) and the Civil Rights Act of 1964. The former was the trigger of the civil rights movement, while the latter was the result of the movement. The report came from a leader of the workers’ organization Fred Halstead, who recorded the bus boycott in Montgomery in 1956. This is a very useful primary source to get to know what happened during the movement.</p>
-        <br />
-        <p>From this, we can find more archives on the Internet to support our arguments further. These archives can focus on different ethnic minority groups and their experiences fighting for their rights. The most significant one is the U.S. National Archives, an independent agency of the U.S. government focusing on the preservation and documentation of government and historical records. When I searched the keyword “Asian American archives” on Google, I found that the National Archives has a special records archive of Asian American and Pacific Islander Records[5]. A visitor can research by topic, go over featured records, and explore further blogs, articles, and scholarly papers. Similarly, there are African American Heritage[6], Hispanic / Latino Heritage[7], etc.</p>
-        <br />
-        <p>Individual archives are also good references and sources helpful to our research. For example, the American Jewish Archives[8] is a very well-organized archive page with a similar structure to the https://www.crmvet.org/. The Jewish Archives collect, preserve, and make available for research, materials on the history of Jews and Jewish communities in the Western Hemisphere (primarily focusing on America) including data of a political, economic, social, cultural, and religious nature. Such an archive provides materials covering widely the topic and can be helpful to research. </p>
-        <br />
-        <p>
-          <img src="../assets/pic/Worknet.png" style="float: left; width: 60%; height: auto; margin-right: 4%; clear: both"><br />
-          Another important category of research sources comes from scholarly papers. I will begin with my focal article and its worknet. The focal article “White Supremacy’s Horcrux and Why the Black Power Movement Almost Destroyed It”, by Dr. Marcus D. Watson[9], focuses on the topic of black power and ethnic identities, especially after the Black Powers Movement in the 1960s. It also tries to discuss why the Black Powers Movement almost destroyed the White Supremacy but at last failed. I found this peer-reviewed focal article on Academic Search Complete, specifically the EBSCOhost website. </p>
-        <br />
-        <p>Meanwhile, the reference papers that the focal article refers to are also worth mentioning. One of the significant references Dr. Watson used to support his argument was “From Negro to Black to African American” by B. L. Martin, published in 1991.[10] B. Martin developed the argument that the transition of African Americans calling themselves, “negro” to “black” and “African American”, was more likely to be supported and motivated by the Black Powers Movement. The core opinion is that it was the Black Powers Movement instead of the Civil Rights Movement that led to this transition and promoted black identity and cohesion and that such a self-identity would greatly improve African Americans’ ethnic self-confidence. </p>
-        <br />
-        <p>The paper “Ontological Terror: Blackness, Nihilism, and Emancipation.” by C. Warren, published in 2018[13], intervenes in Afro-pessimism, Heideggerian metaphysics, and black humanist philosophy by positing that the “Negro question” is intimately imbricated with questions of Being. Antiblackness had become an essential part in white supremacy, and it gave white supremacists the precondition to ground and affirm themselves. Dr. Watson cites this paper to support his argument that white supremacists need to derogate and oppose black people for their self-proud whiteness identities is a part of the foundation ideas of white supremacy, and that is why black people must and almost destroy it. It is the same today for other minority groups, especially Asian Americans. White supremacists were not only towards black people but also any ethnic groups but white people themselves. The racism from white supremacists against minority groups can be a very valuable focus of this topic.</p>
-        <br />
-        <p>Such analysis can be done for many papers, and using such analysis and papers as references can make the research more reliable and highly qualified. And, do not limit yourself to some specified articles. A very important step in doing great research is to widely search for sources to support the arguments. For this topic focusing on ethnic minority groups in U.S. society, we can do further research on, for example, Asian Americans and their civil rights. </p>
-        <br />
-        <p>I found this article when searching the keyword “Asian American Rights” on the EBSCOhost. “The ‘Newest ‘Yellow Peril’’: How American Legal History Shaped The Rise In Asian American Hate Crimes In The COVID-19 Pandemic”, by Adam Minoru Yasui Estacio[11]. This article tries to analyze and explain why Asian immigrants exclusive stereotypes in other Americans’ minds, how American legal history and law bills have excluded Asian Americans, and why and how American society must dismantle the pillars of the perpetual foreigner stereotype by undergoing substantial political, societal, and legal change.</p>
-        <br />
-        <p>Another article focuses more on these “foreigners” or “others’” status and lives in society, especially after some big societal incidents. “The Civil Rights of "Others": Antiterrorism, The Patriot Act, and Arab and South Asian American Rights in Post-9/11 American Society.”, by Vijay Sekhon[12], focuses on South Asian Americans and Arab Americans’ lives and hate crimes towards them after 9/11 terrorism attack. More seriously, the Bush Administration undermined these ethnicities’ freedoms by inserting sections into the Patriot Act. </p>
-        <br />
-        <p>These lists and annotations of research sources and ideas are only a small part of the big research area. However, although the topic is large, we can focus on some core ideas and arguments based on our research target. </p>
+      <div class="paragraph">
+        For this part of the list and annotation of important ideas and sources, I will cover some significant materials that would be helpful for the research on this topic. This part will be divided into two sections. The first are archives that contain rich, valuable, and massive primary sources, which will be very important and useful for research. The second part will be articles, especially scholarly research articles, on this topic. The perspectives of different scholars will give us a new and different opinion, which may give us new inspiration.
+      </div>
+      <figure class="float-left object-pic" >
+        <img src="../assets/pic/objectpic.jpg" alt="Object Picture">
+        <figcaption>Picture Used in Gallery Wall</figcaption>
+      </figure>
+      <div class="paragraph">
+        My research on ethnic minority group rights began from the civil rights movement archive, <a href = "https://www.crmvet.org/">https://www.crmvet.org/</a>. This archive includes a huge amount of primary and secondary sources of this remarkable movement. It was created and maintained by a group of civil rights movement veterans, ensuring that these materials are relatively reliable and valuable. The archive includes four general categories: movement, activists, video/audio, and resources. And within these general categories, photo albums, videos, audio, reports, articles, letters, etc. are provided. To create a gallery wall focusing on the civil rights movement, I selected two law bills, a photo, and a primary report, focusing on the protest of racial segregation. Among these, the most important and direct is the photo, which shows a black girl marching with other people with the sentence “Do not buy segregation”, when protesting a racist local company. The visual impact that such a picture with large characters can be huge. The two law bills include the South segregation laws (also known as Jim Crow Laws) and the Civil Rights Act of 1964. The former was the trigger of the civil rights movement, while the latter was the result of the movement. The report came from a leader of the workers’ organization Fred Halstead, who recorded the bus boycott in Montgomery in 1956. This is a very useful primary source to get to know what happened during the movement.
+      </div>
+      <div class="paragraph">
+        From this, we can find more archives on the Internet to support our arguments further. These archives can focus on different ethnic minority groups and their experiences fighting for their rights. The most significant one is the U.S. National Archives, an independent agency of the U.S. government focusing on the preservation and documentation of government and historical records. When I searched the keyword “Asian American archives” on Google, I found that the National Archives has a special records archive of Asian American and Pacific Islander Records[5]. A visitor can research by topic, go over featured records, and explore further blogs, articles, and scholarly papers. Similarly, there are African American Heritage[6], Hispanic / Latino Heritage[7], etc.
+      </div>
+      <div class="paragraph">
+        Individual archives are also good references and sources helpful to our research. For example, the American Jewish Archives[8] is a very well-organized archive page with a similar structure to the https://www.crmvet.org/. The Jewish Archives collect, preserve, and make available for research, materials on the history of Jews and Jewish communities in the Western Hemisphere (primarily focusing on America) including data of a political, economic, social, cultural, and religious nature. Such an archive provides materials covering widely the topic and can be helpful to research. 
+      </div>
+      <figure class="float-right worknet-pic" >
+        <img src="../assets/pic/Worknet.png" alt="worknet-pic">
+        <figcaption>Worknet Created Based On Focal Article</figcaption>
+      </figure>
+      <div class="paragraph">
+        Another important category of research sources comes from scholarly papers. I will begin with my focal article and its worknet. The focal article <i>“White Supremacy’s Horcrux and Why the Black Power Movement Almost Destroyed It”</i>, by Dr. Marcus D. Watson[9], focuses on the topic of black power and ethnic identities, especially after the Black Powers Movement in the 1960s. It also tries to discuss why the Black Powers Movement almost destroyed the White Supremacy but at last failed. I found this peer-reviewed focal article on Academic Search Complete, specifically the EBSCOhost website. 
+      </div>
+      <div class="paragraph">
+        Meanwhile, the reference papers that the focal article refers to are also worth mentioning. One of the significant references Dr. Watson used to support his argument was <i>“From Negro to Black to African American”</i> by B. L. Martin, published in 1991.[10] B. Martin developed the argument that the transition of African Americans calling themselves, “negro” to “black” and “African American”, was more likely to be supported and motivated by the Black Powers Movement. The core opinion is that it was the Black Powers Movement instead of the Civil Rights Movement that led to this transition and promoted black identity and cohesion and that such a self-identity would greatly improve African Americans’ ethnic self-confidence. 
+      </div>
+      <div class="paragraph">
+        The paper <i>“Ontological Terror: Blackness, Nihilism, and Emancipation.”</i> by C. Warren, published in 2018[13], intervenes in Afro-pessimism, Heideggerian metaphysics, and black humanist philosophy by positing that the “Negro question” is intimately imbricated with questions of Being. Antiblackness had become an essential part in white supremacy, and it gave white supremacists the precondition to ground and affirm themselves. Dr. Watson cites this paper to support his argument that white supremacists need to derogate and oppose black people for their self-proud whiteness identities is a part of the foundation ideas of white supremacy, and that is why black people must and almost destroy it. It is the same today for other minority groups, especially Asian Americans. White supremacists were not only towards black people but also any ethnic groups but white people themselves. The racism from white supremacists against minority groups can be a very valuable focus of this topic.
+      </div>
+      <div class="paragraph">
+        Such analysis can be done for many papers, and using such analysis and papers as references can make the research more reliable and highly qualified. And, do not limit yourself to some specified articles. A very important step in doing great research is to widely search for sources to support the arguments. For this topic focusing on ethnic minority groups in U.S. society, we can do further research on, for example, Asian Americans and their civil rights. 
+      </div>
+      <div class="paragraph">
+        I found this article when searching the keyword “Asian American Rights” on the EBSCOhost. <i>“The ‘Newest ‘Yellow Peril’’: How American Legal History Shaped The Rise In Asian American Hate Crimes In The COVID-19 Pandemic”</i>, by Adam Minoru Yasui Estacio[11]. This article tries to analyze and explain why Asian immigrants exclusive stereotypes in other Americans’ minds, how American legal history and law bills have excluded Asian Americans, and why and how American society must dismantle the pillars of the perpetual foreigner stereotype by undergoing substantial political, societal, and legal change.
+      </div>
+      <div class="paragraph">
+        Another article focuses more on these “foreigners” or “others’” status and lives in society, especially after some big societal incidents. <i>“The Civil Rights of "Others": Antiterrorism, The Patriot Act, and Arab and South Asian American Rights in Post-9/11 American Society.”</i>, by Vijay Sekhon[12], focuses on South Asian Americans and Arab Americans’ lives and hate crimes towards them after 9/11 terrorism attack. More seriously, the Bush Administration undermined these ethnicities’ freedoms by inserting sections into the Patriot Act. 
+      </div>
+      <div class="paragraph">
+        These lists and annotations of research sources and ideas are only a small part of the big research area. However, although the topic is large, we can focus on some core ideas and arguments based on our research target. 
+      </div>
     </div>
 </template>
 
@@ -36,6 +54,40 @@
   height: 100vh;
   margin-left: 8vw;
   margin-right: 8vw;
+  text-align: justify;
+  font-size: 20px;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+figcaption {
+  font-size: 20px;
+  font-weight: 600;
+  text-align: center;
+}
+
+.paragraph {
+  margin-bottom: 1%;
+}
+
+.float-left {
+  float: left;
+  margin: 3%;
+  margin-left: 0;
+}
+
+.float-right {
+  float: right;
+  margin: 3%;
+  margin-right: 0;
+}
+
+.object-pic img {
+  width: 30vw;
+}
+
+.worknet-pic img {
+  height: 45vh;
 }
 
 h1 {
@@ -46,11 +98,11 @@ h1 {
   margin-bottom: 3%;
 }
 
-p {
-  word-wrap: break-word;
+/* p {
+  text-align: justify;
   font-size: 20px;
   overflow-y: auto;
-}
+} */
 
 </style>
 
